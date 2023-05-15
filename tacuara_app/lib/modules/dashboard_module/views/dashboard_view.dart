9041ccafd70051c8438a40a68877e979fe9b0d28
Lorena2387 <lorenaramirez23@hotmail.com>
2023-05-15 13:37:19 -0500

    Modificaciones

--- tacuara_app/lib/modules/dashboard_module/views/dashboard_view.dart
+++ tacuara_app/lib/modules/dashboard_module/views/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:tacuara_app/utils/app_themes.dart';
 import 'package:tacuara_app/utils/images.dart';
 
 class DashboardView extends StatefulWidget {
@@ -35,26 +36,229 @@ class _DashboardViewState extends State<DashboardView> {
       body: ListView(
         padding: const EdgeInsets.all(0),
         children: [
-          Container(
-            color: Colors.red,
-            child: CarouselSlider(
-              items: images
-                  .map((image) => SizedBox(
-                        child: Image.asset(
-                          image,
-                          fit: BoxFit.fitWidth,
-                        ),
-                      ))
-                  .toList(),
-              options: CarouselOptions(
-                height: 300,
-                autoPlay: true,
-                autoPlayCurve: Curves.linear,
-                viewportFraction: 1.0,
-                enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+          SizedBox(
+            width: size.width,
+            child: AspectRatio(
+              aspectRatio: 16 / 12,
+              child: Stack(
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  CarouselSlider(
+                    items: images
+                        .map(
+                          (image) => AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Image.asset(
+                              image,
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      autoPlayCurve: Curves.linear,
+                      viewportFraction: 1.0,
+                      enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional.bottomCenter,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: size.width * 0.04,
+                        vertical: size.height * 0.01,
+                      ),
+                      width: size.width * 0.95,
+                      height: size.height * 0.15,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 10,
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.location_on_rounded,
+                                size: 25,
+                                color: AppThemes.primaryColor,
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              const Expanded(
+                                child: Text(
+                                  'Finca Diosa, vereda San Gerardo\nGarzón-Huila',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.email_outlined,
+                                size: 22,
+                                color: AppThemes.primaryColor,
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              const Expanded(
+                                child: Text(
+                                  'ecohoteltacuara@gmail.com',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.phone_android,
+                                size: 22,
+                                color: AppThemes.primaryColor,
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              const Expanded(
+                                child: Text(
+                                  '324 3298996',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Conoce nuestras habitaciones',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Card(
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: GestureDetector(
+                              onTap: () {
+                                // Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             const CabanaFamiliar()));
+                              },
+                              child: Image(
+                                width: size.width * 0.35,
+                                height: size.width * 0.2,
+                                image: const AssetImage(
+                                  Images.cabanaFamiliar,
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          'Cabaña familiar',
+                          style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Card(
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: GestureDetector(
+                              onTap: () {
+                                // Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             const CabanaParejas()));
+                              },
+                              child: Image(
+                                width: size.width * 0.35,
+                                height: size.width * 0.2,
+                                image: const AssetImage(Images.cabanaParejas3),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          'Cabana parejas',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 13,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
--- tacuara_app/lib/utils/images.dart
+++ tacuara_app/lib/utils/images.dart
@@ -3,4 +3,6 @@ class Images {
   static const String cabana1 = 'assets/images/cabanas1.png';
   static const String cabana2 = 'assets/images/cabanas2.png';
   static const String cabana3 = 'assets/images/cabanas3.png';
+  static const String cabanaFamiliar = 'assets/images/cabana_familiar.png';
+  static const String cabanaParejas3 = 'assets/images/cabana_parejas3.png';
 }
