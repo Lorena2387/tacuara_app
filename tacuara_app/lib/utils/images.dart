fc9c10f6d131ef741f90a124fcdb28cff6586a45
Lorena2387 <lorenaramirez23@hotmail.com>
2023-05-16 16:21:28 -0500

    Modificaciones

--- tacuara_app/lib/utils/images.dart
+++ tacuara_app/lib/utils/images.dart
@@ -7,4 +7,7 @@ class Images {
   static const String cabanaParejas3 = 'assets/images/cabana_parejas3.png';
   static const String tacuara = 'assets/images/tacuara2.png';
   static const String tacuara1 = 'assets/images/tacuara1.png';
+  static const String restaurante = 'assets/images/restaurante.png';
+  static const String pasadia = 'assets/images/pasadia.png';
+  static const String eventos = 'assets/images/eventos.png';
 }
