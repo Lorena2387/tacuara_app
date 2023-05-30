fefccea22b1926893f46a5ec0d9815c844c8f0db
Lorena2387 <lorenaramirez23@hotmail.com>
2023-05-29 23:56:24 -0500

    modificaciones

---
+++ tacuara_app/lib/modules/authentication_module/register_flow/presentation/widgets/user_register_widget.dart
@@ -0,0 +1,14 @@
+class UserRegister {
+  final String name;
+  final String lastname;
+  final String cellphone;
+  final String password;
+  final String confirmpassword;
+
+  UserRegister(
+      {required this.name,
+      required this.lastname,
+      required this.cellphone,
+      required this.password,
+      required this.confirmpassword});
+}
