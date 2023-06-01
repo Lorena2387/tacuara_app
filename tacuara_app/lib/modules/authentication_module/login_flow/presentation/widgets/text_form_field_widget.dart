12d4f58f991adf4d1da583d8a025010503031950
Lorena2387 <lorenaramirez23@hotmail.com>
2023-05-31 22:26:28 -0500

    Modificaciones

--- tacuara_app/lib/modules/authentication_module/login_flow/presentation/widgets/text_form_field_widget.dart
+++ tacuara_app/lib/modules/authentication_module/login_flow/presentation/widgets/text_form_field_widget.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../utils/app_themes.dart';
+
 class TextFormFieldWidget extends StatelessWidget {
-  const TextFormFieldWidget({super.key});
+  final TextEditingController controller;
+  final String labelText;
+  final Function validator;
+  final bool obscureText;
+  const TextFormFieldWidget(
+      {super.key,
+      required this.controller,
+      required this.labelText,
+      required this.validator,
+      required this.obscureText});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(
+        color: AppThemes.primaryColor,
+      ),
+      decoration: InputDecoration(
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black45),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppThemes.primaryColor,
+          ),
+        ),
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+      cursorColor: AppThemes.primaryColor,
+    );
   }
 }
