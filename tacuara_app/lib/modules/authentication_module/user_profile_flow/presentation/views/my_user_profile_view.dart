ed2adb1bc670a08b0376d5337ac7d07cbeda75a8
Lorena2387 <lorenaramirez23@hotmail.com>
2023-06-04 12:20:22 -0500

    modificaciones perfil

--- tacuara_app/lib/modules/authentication_module/user_profile_flow/presentation/views/my_user_profile_view.dart
+++ tacuara_app/lib/modules/authentication_module/user_profile_flow/presentation/views/my_user_profile_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tacuara_app/modules/authentication_module/user_profile_flow/presentation/widgets/text_form_field_profile_widget.dart';
 import 'package:tacuara_app/modules/authentication_module/user_profile_flow/provider/profile_provider.dart';
+import 'package:tacuara_app/utils/app_themes.dart';
+import 'package:tacuara_app/widgets/my_button_widget.dart';
 
 class MyUserProfileView extends StatefulWidget {
   const MyUserProfileView({super.key});
@@ -76,6 +78,63 @@ class _MyUserProfileViewState extends State<MyUserProfileView> {
                     }
                     return null;
                   },
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                TextFormFieldProfileWidget(
+                  controller: controller.identificationNumberController,
+                  labelText: 'Número de identificación',
+                  validator: (value) {
+                    final validNumberRegex = RegExp(r'^\d{8,10}$');
+                    if (!validNumberRegex.hasMatch(value)) {
+                      return 'El número de identificación debe tener entre 8 y 10 dígitos';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                TextFormFieldProfileWidget(
+                  controller: controller.birthdateController,
+                  labelText: 'Fecha de nacimiento',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese su fecha de nacimiento';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                TextFormFieldProfileWidget(
+                  controller: controller.emailController,
+                  labelText: 'Correo electrónico',
+                  validator: (value) {
+                    final emailRegExp =
+                        RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese su correo electrónico';
+                    }
+                    if (!emailRegExp.hasMatch(value)) {
+                      return 'Por favor ingrese un correo válido';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                MyButtonWidget(
+                  onPressed: () {},
+                  color: AppThemes.primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  text: 'Guardar',
+                  width: 220,
+                  height: 40,
                 ),
               ],
             ),
