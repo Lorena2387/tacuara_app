2d20a41c0286ccd4b09efa0addc18e475dbe366a
Lorena2387 <lorenaramirez23@hotmail.com>
2023-06-04 12:35:41 -0500

    pantalla de ajustes del usuario

--- tacuara_app/lib/modules/authentication_module/user_profile_flow/presentation/views/my_settings_view.dart
+++ tacuara_app/lib/modules/authentication_module/user_profile_flow/presentation/views/my_settings_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tacuara_app/utils/app_themes.dart';
 
 class MySettingsView extends StatefulWidget {
   const MySettingsView({super.key});
@@ -10,6 +11,68 @@ class MySettingsView extends StatefulWidget {
 class _MySettingsViewState extends State<MySettingsView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.height * 0.04,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Cambiar mi contraseña',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: AppThemes.primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Términos y condiciones',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: AppThemes.primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Política de privacidad',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: AppThemes.primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.50,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Cerrar sesión',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
