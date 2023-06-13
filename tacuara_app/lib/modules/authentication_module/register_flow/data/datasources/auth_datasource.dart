e3cf455219b440c920ab8979f9454669d1ac7201
Lorena2387 <lorenaramirez23@hotmail.com>
2023-06-13 12:52:17 -0500

    modificaciones

--- tacuara_app/lib/modules/authentication_module/register_flow/data/datasources/auth_datasource.dart
+++ tacuara_app/lib/modules/authentication_module/register_flow/data/datasources/auth_datasource.dart
@@ -1,10 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthDataSource {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<void> registerUser(String email, String password) async {
-    await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
+  Future<void> registerUser(
+      String email, String password, BuildContext context) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Registro exitoso'),
+              content: Text('El usuario ha sido registrado exitosamente'),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Aceptar'))
+              ],
+            );
+          });
+    } catch (e) {
+      throw Exception('Error al registrar al usuario');
+    }
   }
 }
