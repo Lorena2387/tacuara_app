1a5d42d024d49a8b2f15161c38be879d0d255c8c
Lorena2387 <lorenaramirez23@hotmail.com>
2023-06-27 15:31:06 -0500

    showDialog en la cabaña de parejas

--- tacuara_app/lib/modules/room_types/room_flow/views/room_details_view.dart
+++ tacuara_app/lib/modules/room_types/room_flow/views/room_details_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tacuara_app/modules/authentication_module/register_flow/presentation/views/user_register_view.dart';
+import 'package:tacuara_app/utils/app_themes.dart';
+
+//import 'package:tacuara_app/modules/authentication_module/register_flow/presentation/views/user_register_view.dart';
 
 import '../provider/room_provider.dart';
 import '../widgets/cabins_widget.dart';
@@ -40,22 +43,23 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
               cabinServices: controller.familyCabinServices,
               numberOfBeds: '6',
               maximumOccupancy: '10',
-              onPressed: () {
-                if (controller.getUid().toString().isEmpty) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UserRegister()));
-                } else {
-                  showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: Text('Confirma tu reserva'),
-                            content: Text(
-                                'Tu reserva es de una habitación familiar con desayuno, servicio de piscina, parqueadero privado, wifi, televisión, baño privado, cama doble, camarote y cama auxiliar'),
-                          ));
-                }
-              },
+              // onPressed: () {
+              //   if (controller.getUid().toString().isEmpty) {
+              //     Navigator.push(
+              //         context,
+              //         MaterialPageRoute(
+              //             builder: (contex) => const UserRegister()));
+              //   } else {
+              //     showDialog(
+              //         context: context,
+              //         builder: (context) => AlertDialog(
+              //               title: Text('Confirma tu reserva'),
+              //               content: Text(
+              //                   'Tu reserva es de una habitación familiar con todo lo que incluye.'),
+              //             ));
+              //   }
+              // },
+              onPressedButton: () {},
             ),
             CabinsWidget(
               cabinImages: controller.couplesCabinImages,
@@ -64,10 +68,83 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
               cabinServices: controller.couplesCabinServices,
               numberOfBeds: '1',
               maximumOccupancy: '2',
-              onPressed: () {
+              onPressedButton: () {
                 if (controller.getUid().toString().isEmpty) {
-                } else {}
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const UserRegister()));
+                } else {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Confirma tu reserva'),
+                          content: Container(
+                            height: size.height * 0.10,
+                            width: size.width * 0.10,
+                            child: ListView.builder(
+                                itemCount:
+                                    controller.reservationCoupleCabin.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Container(
+                                    width: size.width * 0.05,
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const Icon(
+                                          Icons.circle,
+                                          size: 8,
+                                          color: AppThemes.primaryColor,
+                                        ),
+                                        SizedBox(
+                                          width: size.width * 0.01,
+                                        ),
+                                        Expanded(
+                                            child: Text(
+                                          controller
+                                              .reservationCoupleCabin[index],
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ))
+                                      ],
+                                    ),
+                                  );
+                                }),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Ok'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RoomDetailsView(),
+                                  ),
+                                );
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                          ],
+                        );
+                      });
+                }
               },
+              // onPressed: () {
+              //   if (controller.getUid().toString().isEmpty) {
+              //     Navigator.push(
+              //         context,
+              //         MaterialPageRoute(
+              //             builder: (context) => const UserRegister()));
+              //   } else {}
+              // },
             ),
             SizedBox(
               height: size.height * 0.03,
