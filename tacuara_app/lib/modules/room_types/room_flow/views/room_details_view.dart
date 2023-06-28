d07836347349e17767265bae7644e5465d635352
Lorena2387 <lorenaramirez23@hotmail.com>
2023-06-28 12:05:40 -0500

    detalles de la reservacion de la cabaña familiar

--- tacuara_app/lib/modules/room_types/room_flow/views/room_details_view.dart
+++ tacuara_app/lib/modules/room_types/room_flow/views/room_details_view.dart
@@ -47,23 +47,193 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
               cabinServices: controller.familyCabinServices,
               numberOfBeds: '6',
               maximumOccupancy: '10',
-              // onPressed: () {
-              //   if (controller.getUid().toString().isEmpty) {
-              //     Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //             builder: (contex) => const UserRegister()));
-              //   } else {
-              //     showDialog(
-              //         context: context,
-              //         builder: (context) => AlertDialog(
-              //               title: Text('Confirma tu reserva'),
-              //               content: Text(
-              //                   'Tu reserva es de una habitación familiar con todo lo que incluye.'),
-              //             ));
-              //   }
-              // },
-              onPressedButton: () {},
+              onPressedButton: () {
+                if (controller.getUid().toString().isEmpty) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const UserRegister()));
+                } else {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Confirma tu reserva'),
+                          content: Container(
+                            height: size.height * 0.18,
+                            width: size.width * 0.10,
+                            child: ListView(
+                              children: [
+                                Container(
+                                  width: size.width * 0.05,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        size: 8,
+                                        color: AppThemes.primaryColor,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.01,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          'Check In: ${controller.formatDate(date: calendarController.dateTimeStart)}',
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: size.width * 0.05,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        size: 8,
+                                        color: AppThemes.primaryColor,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.01,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          'Check Out: ${controller.formatDate(date: calendarController.dateTimeEnd)}',
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: size.width * 0.05,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        size: 8,
+                                        color: AppThemes.primaryColor,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.01,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          'Número de noches: ${controller.calculateDays(
+                                            startDate: calendarController
+                                                .dateTimeStart,
+                                            endDate:
+                                                calendarController.dateTimeEnd,
+                                          )}',
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: size.width * 0.05,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        size: 8,
+                                        color: AppThemes.primaryColor,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.01,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          'Tarifa por noche: \$${controller.couplesCabinRate}',
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: size.width * 0.05,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        size: 8,
+                                        color: AppThemes.primaryColor,
+                                      ),
+                                      SizedBox(
+                                        width: size.width * 0.01,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          'Total de la reserva: \$${controller.totalRate(
+                                            totalNights:
+                                                controller.calculateDays(
+                                              startDate: calendarController
+                                                  .dateTimeStart,
+                                              endDate: calendarController
+                                                  .dateTimeEnd,
+                                            ),
+                                            dayRate:
+                                                controller.couplesCabinRate,
+                                          )}',
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RoomDetailsView(),
+                                  ),
+                                );
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Ok'),
+                            ),
+                          ],
+                        );
+                      });
+                }
+              },
             ),
             CabinsWidget(
               cabinImages: controller.couplesCabinImages,
@@ -239,10 +409,6 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
                           ),
                           actions: [
                             TextButton(
-                              onPressed: () {},
-                              child: const Text('Cancelar'),
-                            ),
-                            TextButton(
                               onPressed: () {
                                 Navigator.push(
                                   context,
@@ -252,6 +418,10 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
                                   ),
                                 );
                               },
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {},
                               child: const Text('Ok'),
                             ),
                           ],
@@ -259,14 +429,6 @@ class _RoomDetailsViewState extends State<RoomDetailsView> {
                       });
                 }
               },
-              // onPressed: () {
-              //   if (controller.getUid().toString().isEmpty) {
-              //     Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //             builder: (context) => const UserRegister()));
-              //   } else {}
-              // },
             ),
             SizedBox(
               height: size.height * 0.03,
