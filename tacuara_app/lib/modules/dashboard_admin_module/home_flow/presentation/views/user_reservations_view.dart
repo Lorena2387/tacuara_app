86206610294893ab762e4d8ceb079d5ce0f51215
Lorena2387 <lorenaramirez23@hotmail.com>
2023-07-05 00:22:04 -0500

    modificaciones reservaciones de usuarios

--- tacuara_app/lib/modules/dashboard_admin_module/home_flow/presentation/views/user_reservations_view.dart
+++ tacuara_app/lib/modules/dashboard_admin_module/home_flow/presentation/views/user_reservations_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tacuara_app/modules/dashboard_admin_module/home_flow/presentation/widgets/reservation_card.dart';
+//import 'package:tacuara_app/modules/dashboard_admin_module/home_flow/domain/reservation.dart';
+//import 'package:tacuara_app/modules/dashboard_admin_module/home_flow/presentation/widgets/reservation_card.dart';
 
 class UserReservations extends StatefulWidget {
   const UserReservations({super.key});
@@ -11,26 +12,7 @@ class UserReservations extends StatefulWidget {
 class _UserReservationsState extends State<UserReservations> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-        body: ListView(
-      children: [
-        SizedBox(
-          height: size.height * 0.02,
-        ),
-        const ReservationCardWidget(
-          referenceNumber: 'F200723',
-          checkIn: '20 de julio de 2023',
-          checkOut: '22 de julio de 2023',
-          nightRate: '\$490.000',
-          roomType: 'Cabaña familiar',
-          totalRate: '\$1.470.000',
-          name: 'Daniel Cerquera',
-          email: 'danielcerquera@gmail.com',
-          cellphone: '3157394827',
-          totalNight: '3',
-        ),
-      ],
-    ));
+    //Size size = MediaQuery.of(context).size;
+    return Scaffold();
   }
 }
