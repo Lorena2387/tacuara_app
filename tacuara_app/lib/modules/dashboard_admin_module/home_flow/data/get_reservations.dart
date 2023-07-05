00e746986513e9abd5e777a887fa25248e40fb6a
Lorena2387 <lorenaramirez23@hotmail.com>
2023-07-05 00:22:21 -0500

    reservaciones de usuarios

--- tacuara_app/lib/modules/dashboard_admin_module/home_flow/data/get_reservations.dart
+++ tacuara_app/lib/modules/dashboard_admin_module/home_flow/data/get_reservations.dart
@@ -2,12 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tacuara_app/modules/dashboard_admin_module/home_flow/domain/reservation.dart';
 
 class GetReservations {
-  static Future<void> listReservation(
-      {required Reservation reservation}) async {
-    final getReservation =
+  Future<Reservation?> readReservation() async {
+    final reservation =
         FirebaseFirestore.instance.collection('reservation').doc();
-    await getReservation.get().then((querySnapshop) {
-      print('Firestore: ${querySnapshop}');
-    });
+    final snapshop = await reservation.get();
+    if (snapshop.exists) {
+      return Reservation.fromJson(snapshop.data()!);
+    }
+    return null;
   }
 }
